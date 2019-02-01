2f8b927c68276ce1cb9efe6f0ed63d0f51c9de46
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2019-02-01 15:45:02 +0530

    Initial commit

---
+++ .gocd/build.gocd.groovy
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+GoCD.script {
+  environments {
+    environment('gocd') {
+      pipelines = ['code-sign']
+    }
+  }
+
+  pipelines {
+    pipeline('code-sign') {
+      group = 'go-cd'
+      materials() {
+        git {
+          url = 'https://github.com/ketan/codesigning'
+        }
+        dependency {
+          pipeline = 'installers'
+          stage = 'dist'
+        }
+      }
+
+      stages {
+        stage('sign') {
+          jobs {
+            ['rpm', 'deb'].collect { osType ->
+              job(osType) {
+                tasks {
+                  fetchDirectory {
+                    pipeline = 'installers'
+                    stage = 'dist'
+                    job = 'dist'
+                    source = "dist/${osType}"
+                  }
+                  exec { commandLine = ["rake", "--trace", "${osType}:sign"] }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
